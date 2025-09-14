94763f5846e943ea0f4d3be283d09908d8b563c9
TheTrueB0B <Benjamin.G.Jewell@Gmail.com>
2025-09-14 14:42:02 -0500

    adds Weston Moore joker, without texture

---
+++ moore.lua
@@ -0,0 +1,27 @@
+-- Weston Moore
+SMODS.Joker {
+    key = "weston_moore",
+    pos = { x = 0, y = 0 },
+    rarity = 2,
+    blueprint_compat = true,
+    cost = 7,
+    discovered = true,
+    config = { extra = { mult = 7 }, },
+    loc_txt = {
+        name = "Weston Moore",
+        text = {
+            "Each played {C:attention}7{} gives",
+            "{C:red,s:1.1}+#1#{} Mult",
+        },
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and context.other_card:get_id() == 7 then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+    end
+}
